d3ec5d951012db7a1baa5b6d52a13c45f90505b4
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-12-12 18:10:24 +0500

    half done

--- number.py
+++ number.py
@@ -9,7 +9,7 @@ class Number:
 
         returns: int
         """
-        pass
+        return self.value
 
     def is_odd(self):
         """
@@ -18,7 +18,10 @@ class Number:
         returns: bool
 
         """
-        pass
+        if self.value % 2 == 1:
+            return True
+        else:
+            return False
 
     def is_even(self):
         """
@@ -26,7 +29,10 @@ class Number:
 
         returns: bool
         """
-        pass
+        if self.value % 2 == 0:
+            return True
+        else:
+            return False
 
     def is_prime(self):
         """
@@ -34,7 +40,15 @@ class Number:
 
         returns: bool
         """
-        pass
+        count = 0
+        n = self.value
+        for i in range(1,n+1):
+            if n % i == 0:
+                count += 1
+        if count == 2:
+            return True
+        else:
+            return False
 
     def get_divisors(self):
         """
@@ -42,7 +56,12 @@ class Number:
 
         returns: list
         """
-        pass
+        divisors = []
+        n = self.value
+        for i in range(1,n+1):
+            if n % i == 0:
+                divisors.append(i)
+        return divisors
 
     def get_length(self):
         """
@@ -50,7 +69,14 @@ class Number:
 
         returns: int
         """
-        pass
+        n = self.value
+        count = 0
+        i = 0
+        while n >= i:
+            count += 1
+            n //= 10
+            i += 1
+        return count
 
     def get_sum(self):
         """
@@ -58,7 +84,14 @@ class Number:
 
         returns: int
         """
-        pass
+        n = self.value
+        s = 0
+        i = 0
+        while n >= i:
+            s += n%10
+            n //= 10
+            i+=1
+        return s
 
     def get_reverse(self):
         """
@@ -66,7 +99,8 @@ class Number:
 
         returns: int
         """
-        pass
+        n = self.value
+        return int(str(n)[::-1])
 
     def is_palindrome(self):
         """
@@ -74,7 +108,11 @@ class Number:
 
         returns: bool
         """
-        pass
+        n = self.value
+        if n == (str(n)[::-1]):
+            return True
+        else:
+            return False
 
     def get_digits(self):
         """
@@ -82,7 +120,17 @@ class Number:
 
         returns: list
         """
-        pass
+        n = self.value
+        digits = []
+        i = 0
+        s = 0
+        while n >= i:
+            s = n % 10
+            digits.append(s)
+            n //= 10
+            i += 1
+        digits.reverse()
+        return digits
 
     def get_max(self):
         """
@@ -134,4 +182,20 @@ class Number:
     
 
 # Create a new instance of Number
-number = Number(3)
+number = Number(13)
+print(number.get_number())
+print(number.is_odd())
+print(number.is_even())
+print(number.is_prime())
+print(number.get_divisors())
+print(number.get_length())
+print(number.get_sum())
+print(number.get_reverse())
+print(number.is_palindrome())
+print(number.get_digits())
+print(number.get_max())
+print(number.get_min())
+print(number.get_average())
+print(number.get_median())
+print(number.get_range())
+print(number.get_frequency())
